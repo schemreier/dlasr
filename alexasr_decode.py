9a036f4a81db35e202a107ea72a2151ec834beeb
Emre Yilmaz <emrey@kth.se>
2017-09-04 15:08:11 +0200

    Naming convention fixed..

--- alexasr_decode.py
+++ alexasr_decode.py
@@ -3,6 +3,7 @@
 from alex_asr import Decoder
 import wave, pdb, glob, os, sys
 import struct
+from os.path import basename
 
 inputdir=sys.argv[1]
 scratchdir=sys.argv[2]
@@ -14,8 +15,7 @@ os.system('mkdir -p '+scratchdir)
 
 for inputfile in glob.glob(inputdir+'/*.wav'):
   decoder = Decoder(resourcedir+"/asr_model_dir_nnet3")
-  file_id = inputfile.split('/')[-1].replace('.wav','')
-  ref_trans = inputfile.split('/')[-1].replace('.wav','').split('_')[2]
+  file_id = basename(inputfile)
   os.system('sox '+inputfile+' -e signed-integer -r 16000 -b 16 '+scratchdir+file_id+'.wav')
   # Load audio frames from input wav file.
   data = wave.open(scratchdir+file_id+'.wav')
